10bbe5e2f80db906c705f3def32f4df7937a2347
Maxim Dokuchaev <mixamus@icloud.com>
2021-02-01 13:48:10 +0300

    add todo rest

--- src/main/java/com/apress/todo/TodoClientApplication.java
+++ src/main/java/com/apress/todo/TodoClientApplication.java
@@ -19,24 +19,29 @@ public class TodoClientApplication {
         app.run(args);
     }
 
-    private Logger log = LoggerFactory.getLogger
-            (TodoClientApplication.class);
+    private Logger log = LoggerFactory.getLogger(TodoClientApplication.class);
 
     @Bean
     public CommandLineRunner process(ToDoRestClient client) {
+
         return args -> {
+
             Iterable<ToDo> toDos = client.findAll();
             assert toDos != null;
             toDos.forEach(toDo -> log.info(toDo.toString()));
+
             ToDo newToDo = client.upsert(new ToDo("Drink plenty of Water daily!"));
             assert newToDo != null;
             log.info(newToDo.toString());
+
             ToDo toDo = client.findById(newToDo.getId());
             assert toDos != null;
             log.info(toDo.toString());
+
             ToDo completed = client.setCompleted(newToDo.getId());
             assert completed.isCompleted();
             log.info(completed.toString());
+
             client.delete(newToDo.getId());
             assert client.findById(newToDo.getId()) == null;
         };
--- src/main/java/com/apress/todo/client/ToDoRestClient.java
+++ src/main/java/com/apress/todo/client/ToDoRestClient.java
@@ -17,25 +17,22 @@ import java.util.Map;
 
 @Service
 public class ToDoRestClient {
+
     private RestTemplate restTemplate;
     private ToDoRestClientProperties properties;
 
-    public ToDoRestClient(
-            ToDoRestClientProperties properties) {
+    public ToDoRestClient(ToDoRestClientProperties properties) {
         this.restTemplate = new RestTemplate();
-        this.restTemplate.setErrorHandler(
-                new ToDoErrorHandler());
+        this.restTemplate.setErrorHandler(new ToDoErrorHandler());
         this.properties = properties;
     }
 
     public Iterable<ToDo> findAll() throws URISyntaxException {
-        RequestEntity<Iterable<ToDo>> requestEntity = new RequestEntity
-                <Iterable<ToDo>>(HttpMethod.GET, new URI(properties.getUrl() +
-                properties.getBasePath()));
+        RequestEntity<Iterable<ToDo>> requestEntity = new RequestEntity<>(HttpMethod.GET, new URI(
+                properties.getUrl() + properties.getBasePath()));
         ResponseEntity<Iterable<ToDo>> response =
-                restTemplate.exchange(requestEntity, new
-                        ParameterizedTypeReference<Iterable<ToDo>>() {
-                        });
+                restTemplate.exchange(requestEntity, new ParameterizedTypeReference<Iterable<ToDo>>() {
+                });
         if (response.getStatusCode() == HttpStatus.OK) {
             return response.getBody();
         }
@@ -43,18 +40,17 @@ public class ToDoRestClient {
     }
 
     public ToDo findById(String id) {
-        Map<String, String> params = new HashMap<String, String>();
+        Map<String, String> params = new HashMap<>();
         params.put("id", id);
         return restTemplate.getForObject(properties.getUrl() +
                 properties.getBasePath() + "/{id}", ToDo.class, params);
     }
 
     public ToDo upsert(ToDo toDo) throws URISyntaxException {
-        RequestEntity<?> requestEntity = new RequestEntity<>(toDo, HttpMethod.
-                POST, new URI(properties.getUrl() + properties.getBasePath()));
-        ResponseEntity<?> response = restTemplate.exchange(requestEntity, new
-                ParameterizedTypeReference<ToDo>() {
-                });
+        RequestEntity<?> requestEntity = new RequestEntity<>(toDo, HttpMethod.POST, new URI(
+                properties.getUrl() + properties.getBasePath()));
+        ResponseEntity<?> response = restTemplate.exchange(requestEntity, new ParameterizedTypeReference<ToDo>() {
+        });
         if (response.getStatusCode() == HttpStatus.CREATED) {
             return restTemplate.getForObject(response.getHeaders().getLocation(), ToDo.class);
         }
@@ -62,16 +58,16 @@ public class ToDoRestClient {
     }
 
     public ToDo setCompleted(String id) throws URISyntaxException {
-        Map<String, String> params = new HashMap<String, String>();
+        Map<String, String> params = new HashMap<>();
         params.put("id", id);
-        restTemplate.postForObject(properties.getUrl() + properties.getBasePath() + "/{id}?_method=patch", null, ResponseEntity.class, params);
+        restTemplate.postForObject(properties.getUrl() + properties.getBasePath() + "/{id}?_method=patch",
+                null, ResponseEntity.class, params);
         return findById(id);
     }
 
     public void delete(String id) {
-        Map<String, String> params = new HashMap<String, String>();
+        Map<String, String> params = new HashMap<>();
         params.put("id", id);
-        restTemplate.delete(properties.getUrl() + properties.getBasePath() +
-                "/{id}", params);
+        restTemplate.delete(properties.getUrl() + properties.getBasePath() + "/{id}", params);
     }
 }
